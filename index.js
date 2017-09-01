ab00c72e20bb0466ed7192cd1e2db47caa8c1148
Steve <steven@visarc.uk>
2017-09-01 14:04:04 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const viewports = [1600, 1000, 800, 600, 500];
+
+  await page.goto('#url'); // Add url
+
+  for(let i=0; i < viewports.length; i++) {
+    let vw = viewports[i];
+
+    // Height doesn't matter, screenshotting full page.
+    await page.setViewport({
+      width: vw,
+      height: 1000
+    });
+
+    await page.screenshot({
+      path: `/screenshots/screen-${vw}.png`,
+      fullPage: true
+    });
+  }
+
+  browser.close();
+})();
